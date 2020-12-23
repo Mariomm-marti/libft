c525da4e3485697873e4b92584fe2b46cceee56b
Mario Martin moreno <mmartin-@student.42madrid.com>
2020-12-23 13:29:01 +0100

    Improved mat44_inverse performance without using a temporary variable, and removed main

--- srcs/mat44_inverse.c
+++ srcs/mat44_inverse.c
@@ -6,7 +6,7 @@
 /*   By: mmartin- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 16:17:09 by mmartin-          #+#    #+#             */
-/*   Updated: 2020/12/22 22:10:46 by mmartin-         ###   ########.fr       */
+/*   Updated: 2020/12/23 13:28:24 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ static double	mat44_det33(t_vec3 const a, t_vec3 const b, t_vec3 const c)
 static void		mat44_adjoint(t_mat44 out, t_mat44 const a)
 {
 	char const	tab[4][3] = {{1, 2, 3}, {0, 2, 3}, {0, 1, 3}, {0, 1, 2}};
-	t_mat44		temp;
 	char		i;
 	char		j;
 
@@ -33,7 +32,7 @@ static void		mat44_adjoint(t_mat44 out, t_mat44 const a)
 	while (++i < 4 && (j = -1))
 		while (++j < 4)
 		{
-			temp[i][j] = mat44_det33((t_vec3){
+			out[j][i] = mat44_det33((t_vec3){
 					a[tab[i][0]][tab[j][0]],
 					a[tab[i][0]][tab[j][1]],
 					a[tab[i][0]][tab[j][2]]},
@@ -46,9 +45,8 @@ static void		mat44_adjoint(t_mat44 out, t_mat44 const a)
 					a[tab[i][2]][tab[j][1]],
 					a[tab[i][2]][tab[j][2]]});
 			if ((i + j) % 2)
-				temp[i][j] = -temp[i][j];
+				out[j][i] = -out[j][i];
 		}
-	mat44_transpose(out, temp);
 }
 
 /*
@@ -76,21 +74,3 @@ int				mat44_inverse(t_mat44 out, t_mat44 const a)
 			out[i][j] = out[i][j] * matrix_determinant;
 	return (1);
 }
-
-int		main(int argc, char **argv)
-{
-	t_mat44		val = {{1, 9, 4, 6}, {-1, 3, 9.1157, 81.1}, {8325, -185, -17, -0.19}, {0, 0, 19, 4}};
-	t_mat44		out;
-
-	printf("%-20lf %-20lf %-20lf %-20lf\n", val[0][0], val[1][0], val[2][0], val[3][0]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", val[0][1], val[1][1], val[2][1], val[3][1]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", val[0][2], val[1][2], val[2][2], val[3][2]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", val[0][3], val[1][3], val[2][3], val[3][3]);
-
-	printf("Has inverse? %s\n", mat44_inverse(out, val) ? "true" : "false");
-	printf("%-20lf %-20lf %-20lf %-20lf\n", out[0][0], out[1][0], out[2][0], out[3][0]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", out[0][1], out[1][1], out[2][1], out[3][1]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", out[0][2], out[1][2], out[2][2], out[3][2]);
-	printf("%-20lf %-20lf %-20lf %-20lf\n", out[0][3], out[1][3], out[2][3], out[3][3]);
-}
-
